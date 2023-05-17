da67172732fb7f6ddcce3f4e599bdffe39015eba
joalisonpereira <joalison.pereira@hotmail.com>
2023-05-17 09:56:45 -0300

    implementing grid tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">CSS Grid</div>;
+  return (
+    <div className="app">
+      <header className="center">header</header>
+      <main className="center">main</main>
+      <aside className="center">aside</aside>
+      <footer className="center">footer</footer>
+    </div>
+  );
 }
 
 export default App;
